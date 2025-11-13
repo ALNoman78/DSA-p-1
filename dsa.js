ecd5e2b310fda7e3f95fae82b6fe839553599cba
ALNoman78 <abdullaalno078@gmail.com>
2025-11-13 23:42:24 +0600

    stateful and stateless method

--- dsa.js
+++ dsa.js
@@ -1,1 +1,27 @@
-console.log("dsa")
+//  stateless and stateful
+
+const counter = (amount) => {
+    let count = 0;
+    count = count + amount
+
+    return count;
+}
+
+// console.log(counter(4))
+// console.log(counter(2));
+
+const counterTwo = {
+    count: 0,
+
+    add(amount) {                     //? inside object all the function is called method
+        this.count = this.count + amount
+    },
+    print() {
+        console.log(count);
+    },
+}
+
+counter.add(3)
+counter.add(3)
+
+counter.print()
